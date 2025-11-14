e95637584d0a9236f2156291a71c0060128cbc34
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-11-14 16:01:54 +0530

    Time: 142 ms (96.24%) | Memory: 39.8 MB (49.77%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
@@ -1,6 +1,5 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        # diff matrix must be (n+1) x (n+1)
         diff = [[0] * (n + 1) for _ in range(n + 1)]
 
         for r1, c1, r2, c2 in queries:
@@ -9,19 +8,16 @@ class Solution:
             diff[r2 + 1][c1] -= 1
             diff[r2 + 1][c2 + 1] += 1
 
-        # Prefix row-wise
         for r in range(n):
             curr = 0
             for c in range(n):
                 curr += diff[r][c]
-                diff[r][c] = curr     # fixed: = not ==
+                diff[r][c] = curr    
 
-        # Prefix column-wise
         for c in range(n):
             curr = 0
             for r in range(n):
                 curr += diff[r][c]
                 diff[r][c] = curr
 
-        # return n x n matrix
         return [row[:n] for row in diff[:n]]

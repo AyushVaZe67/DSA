cee01fe1c3d2ee06990c352f2a8c835743341a15
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-24 13:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (98.47%) - LeetSync

---
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        total = sum(apple)
+        capacity.sort(reverse=True)
+
+        curr = 0
+        for i,c in enumerate(capacity):
+            curr += c
+            if curr >= total:
+                return i + 1
+
+        return len(capacity)

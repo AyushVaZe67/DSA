8181770eea2a36ba1d05529174ceb396055a311d
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2026-01-03 15:04:02 +0530

    Time: 7 ms (83.84%) | Memory: 17.5 MB (88.72%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        MOD = 10**9 + 7
+        aba, abc = 6, 6
+        for _ in range(1, n):
+            new_aba = (3 * aba + 2 * abc) % MOD
+            new_abc = (2 * aba + 2 * abc) % MOD
+            aba, abc = new_aba, new_abc
+        return (aba + abc) % MOD

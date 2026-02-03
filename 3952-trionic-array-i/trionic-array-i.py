14a8f2620250fcfee48f56d441af9a5ffd65a6cc
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2026-02-03 18:11:32 +0530

    Time: 11 ms (69.30%) | Memory: 19.5 MB (19.11%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.py
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -6,23 +6,18 @@ class Solution:
         if n < 4:
             return False
         
-        # prefix_inc[i]: nums[0..i] strictly increasing
         prefix_inc = [True] * n
         for i in range(1, n):
             prefix_inc[i] = prefix_inc[i-1] and (nums[i-1] < nums[i])
         
-        # suffix_inc[i]: nums[i..n-1] strictly increasing
         suffix_inc = [True] * n
         for i in range(n-2, -1, -1):
             suffix_inc[i] = (nums[i] < nums[i+1]) and suffix_inc[i+1]
         
-        # Check all valid p, q
         for p in range(1, n-1):
             if not prefix_inc[p]:
                 continue
-            # Try all possible q
             for q in range(p+1, n-1):
-                # Check decreasing from p to q
                 dec_ok = True
                 for i in range(p, q):
                     if nums[i] <= nums[i+1]:

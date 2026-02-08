ecce5c0bc2e77768715141255e23f480a956e2da
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2026-02-08 23:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (11.68%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(node):
+            if not node:
+                return 0  # Height of empty tree is 0
+            
+            left_height = check(node.left)
+            if left_height == -1:
+                return -1  # Left subtree is unbalanced
+            
+            right_height = check(node.right)
+            if right_height == -1:
+                return -1  # Right subtree is unbalanced
+            
+            if abs(left_height - right_height) > 1:
+                return -1  # Current node is unbalanced
+            
+            return 1 + max(left_height, right_height)
+        
+        return check(root) != -1

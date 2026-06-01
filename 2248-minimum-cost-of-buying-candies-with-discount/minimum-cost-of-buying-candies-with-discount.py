b6c742c4adc461e16c61b087175491b8e800d45e
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2026-06-01 22:47:46 +0530

    Time: 4 ms (9.89%) | Memory: 19.3 MB (48.06%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse=True)
+        
+        total = 0
+        n = len(cost)
+        
+        for i in range(n):
+            if i % 3 != 2:
+                total += cost[i]
+        
+        return total
